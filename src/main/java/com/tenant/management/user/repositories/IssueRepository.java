1bacbd702145005f13d38f264fe4a390623e9769
suppixie <mamthapatalay6@gmail.com>
2024-12-07 17:25:32 +0000

    Created Admin and Issue APIs

---
+++ src/main/java/com/tenant/management/user/repositories/IssueRepository.java
@@ -0,0 +1,15 @@
+package com.tenant.management.user.repositories;
+
+import com.tenant.management.user.entities.Issue;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+@Repository
+public interface IssueRepository extends JpaRepository<Issue,UUID> {
+    @Query(value = "SELECT * FROM issue where issue.issueId =?1", nativeQuery = true)
+    Optional<Issue> findByUuid(UUID issueId);
+
+}
